4d366ed56db41a3302966653081f9dbcd4c2f765
Simon Seyock <seyock@terrestris.de>
2021-07-22 15:24:44 +0200

    Proper error message if no geocode config is available.

--- src/modules/geocoder/geocoder-service.js
+++ src/modules/geocoder/geocoder-service.js
@@ -157,12 +157,12 @@ angular.module('anol.geocoder')
                             return;
                         }
                         geocoder = this.getGeocoder(configName);
-                    } else {
+                    } else if (this.getUrlGeocodeConfigs().length > 0) {
                         geocoder = this.getGeocoder(this.getUrlGeocodeConfigs()[0].name);
                     }
 
                     if (!geocoder) {
-                        console.error('No geocode config available.')
+                        console.error('No search config for url gecode available (missing configuration with `availableInUrlGeocode` option).')
                         return;
                     }
 
