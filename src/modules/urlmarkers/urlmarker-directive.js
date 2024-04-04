60b9a04fda48b2d5ac0cb56f13580f4c6829fff9
Simon Seyock <seyock@terrestris.de>
2024-04-04 09:29:09 +0200

    fix: make multiple markers closable and re-openable

--- src/modules/urlmarkers/urlmarker-directive.js
+++ src/modules/urlmarkers/urlmarker-directive.js
@@ -10,34 +10,53 @@ angular.module('anol.urlmarkers')
                 return;
             }
 
-            var overlays = [];
+            const popupTemplate = '<div class="anol-popup bottom">' +
+                '<span class="anol-popup-closer glyphicon glyphicon-remove" ng-mousedown="$event.stopPropagation();"></span>' +
+                '<div class="anol-popup-content" bbcode>' +
+                '</div>' +
+                '</div>';
 
-            var popupTemplate = '<div class="anol-popup bottom">' +
-                            '<span class="anol-popup-closer glyphicon glyphicon-remove" ng-mousedown="$event.stopPropagation();"></span>' +
-                            '<div class="anol-popup-content" bbcode>' +
-                            '</div>' +
-                            '</div>';
+            const featuresWithPopup = [];
+
+            const displayPopup = (feature) => {
+                if (feature.get('label')) {
+                    const overlayTemplate = angular.element(angular.copy(popupTemplate));
+                    overlayTemplate.find('.anol-popup-content').text(feature.get('label'));
+                    const overlayElement = $compile(overlayTemplate)(scope);
+                    const overlay = new Overlay({
+                        element: overlayElement[0],
+                        autoPan: false
+                    });
+                    overlayElement.find('.anol-popup-closer').click(function() {
+                        MapService.getMap().removeOverlay(overlay);
+                        featuresWithPopup.splice(featuresWithPopup.indexOf(feature), 1);
+                    });
+                    angular.element(overlay.getElement()).parent().addClass('anol-popup-container');
+                    MapService.getMap().addOverlay(overlay);
+
+                    overlay.setPosition(getCenter(feature.getGeometry().getExtent()));
+                    featuresWithPopup.push(feature);
+                }
+            }
 
             scope.$watchCollection(() => UrlMarkerService.getFeatures(), function () {
-                for (const feature of UrlMarkerService.getFeatures()) {
-                    if (feature.get('label')) {
-                        var overlayTemplate = angular.element(angular.copy(popupTemplate));
-                        overlayTemplate.find('.anol-popup-content').text(feature.get('label'));
-                        var overlayElement = $compile(overlayTemplate)(scope);
-                        var overlay = new Overlay({
-                            element: overlayElement[0],
-                            autoPan: false
-                        });
-                        overlayElement.find('.anol-popup-closer').click(function() {
-                            MapService.getMap().removeOverlay(overlay);
-                        });
-                        angular.element(overlay.getElement()).parent().addClass('anol-popup-container');
-                        MapService.getMap().addOverlay(overlay);
-
-                        overlay.setPosition(getCenter(feature.getGeometry().getExtent()));
-                        overlays.push(overlay);
-                    }
+                const features = UrlMarkerService.getFeatures();
+                for (const feature of features) {
+                    displayPopup(feature);
                 }
+
+                const map = MapService.getMap();
+                map.on('click', e => {
+                    const clicked = map.getFeaturesAtPixel(e.pixel, {
+                        hitTolerance: 10
+                    });
+                    const feature = clicked.find(f =>
+                        features.includes(f) && !featuresWithPopup.includes(f)
+                    );
+                    if (feature) {
+                        displayPopup(feature);
+                    }
+                });
             });
         };
     }]);
