390b3f3d168571143da63e927c6f8f39aa4e92bc
jansule <suleiman@terrestris.de>
2025-08-14 13:25:22 +0200

    Revert "fix: handle visibility change of top-level combined layers"
    This reverts commit 6e589ce468f619aa71a0c3b81668765099f3ea7c.

--- src/anol/layer.js
+++ src/anol/layer.js
@@ -162,9 +162,7 @@ class AnolBaseLayer {
         // For combined layers, visibility depends on at least
         // one other layer in combination being visible.
         let olLayerVisible = visible;
-        // Top-level layers might be combined while not being
-        // in a group.
-        if (this.combined && this.hasGroup()) {
+        if (this.combined) {
             olLayerVisible = this.anolGroup.layers.some(l => l.getVisible());
         }
         this.olLayer.setVisible(olLayerVisible);
