133ac6cf8863170098d44a521d4db891a35ce42e
Noémie Treff <ntreff@terrestris.de>
2025-09-30 14:30:07 +0200

    fix: adjust transparency slider

--- src/modules/transparencysettings/module.js
+++ src/modules/transparencysettings/module.js
@@ -1,3 +1,27 @@
 import { anol } from '../../anol/anol.js';
 
-angular.module('anol.transparencysettings', []);
+angular.module('anol.transparencysettings', [])
+    .service('TransparencyDialogService', function() {
+        let activeDialog = null;
+
+        return {
+          openDialog: function(dialogId) {
+            activeDialog = dialogId;
+          },
+          closeDialog: function(dialogId) {
+            if (activeDialog === dialogId) {
+              activeDialog = null;
+            }
+          },
+          toggleDialog: function(dialogId) {
+            if (activeDialog === dialogId) {
+              activeDialog = null;
+            } else {
+              activeDialog = dialogId;
+            }
+          },
+          isOpen: function(dialogId) {
+            return activeDialog === dialogId;
+          }
+        };
+      });
